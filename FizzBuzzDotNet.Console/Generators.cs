8510400b7d959ea37238f22dc967ff6c8c9520cc
Jeffrey Rivor <jeffreyrivor@gmail.com>
2020-05-18 00:59:57 -0700

    Precompute and cache generating functions

---
+++ FizzBuzzDotNet.Console/Generators.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Primitives;
+
+namespace FizzBuzzDotNet
+{
+    static class Generators
+    {
+        private static readonly StringSegment _fizzBuzz = new StringSegment("FizzBuzz");
+
+        private static readonly StringSegment _fizz = _fizzBuzz.Subsegment(0, 4);
+
+        private static readonly StringSegment _buzz = _fizzBuzz.Subsegment(4);
+
+        public static StringSegment Default(int i) => i.ToString();
+
+        public static StringSegment Fizz(int _) => _fizz;
+
+        public static StringSegment Buzz(int _) => _buzz;
+
+        public static StringSegment FizzBuzz(int _) => _fizzBuzz;
+    }
+}
--- FizzBuzzDotNet.Console/Program.cs
+++ FizzBuzzDotNet.Console/Program.cs
@@ -8,38 +8,34 @@ namespace FizzBuzzDotNet
 {
     class Program
     {
+        static Func<int, StringSegment>[] GeneratorMap = new Func<int, StringSegment>[]
+        {
+            Generators.FizzBuzz,
+            Generators.Default,
+            Generators.Default,
+            Generators.Fizz,
+            Generators.Default,
+            Generators.Buzz,
+            Generators.Fizz,
+            Generators.Default,
+            Generators.Default,
+            Generators.Fizz,
+            Generators.Buzz,
+            Generators.Default,
+            Generators.Fizz,
+            Generators.Default,
+            Generators.Fizz,
+        };
+
         static void Main(string[] args)
         {
-            var fizzBuzz = new StringSegment("FizzBuzz");
-
             var results = new List<StringSegment>(10000000);
 
             Stopwatch stopwatch = Stopwatch.StartNew();
 
             foreach (var i in Enumerable.Range(1, 10000000))
             {
-                var idx = 4; // 'B' at beginning of 'Buzz' in char array
-                var count = 0; // no letters to print from char array
-
-                if (i % 3 == 0)
-                {
-                    idx = 0; // 'F' at beginning of 'Fizz' in char array
-                    count += 4; // letters to print = 'Fizz'
-                }
-
-                if (i % 5 == 0)
-                {
-                    count += 4; // add letters to print = 'Buzz'; when both i % 3 and i % 5, letters to print = 'FizzBuzz'
-                }
-
-                if (count > 0)
-                {
-                    results.Add(fizzBuzz.Subsegment(idx, count));
-                }
-                else
-                {
-                    results.Add(i.ToString());
-                }
+                results.Add(GeneratorMap[i % GeneratorMap.Length](i));
             }
 
             stopwatch.Stop();
